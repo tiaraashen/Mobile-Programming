5637ae32bef54902fa368de35ec4c76c33cc5aea
tiaraashen <tpanggraeni01@gmail.com>
2020-10-05 02:38:13 +0700

    4

---
+++ BelajarActivity/app/src/main/java/pens/lab/app/belajaractivity/modul/profile/ProfileActivity.java
@@ -0,0 +1,28 @@
+package pens.lab.app.belajaractivity.modul.profile;
+
+import android.view.View;
+
+import pens.lab.app.belajaractivity.base.BaseFragmentHolderActivity;
+
+
+public class ProfileActivity extends BaseFragmentHolderActivity {
+    ProfileFragment profileFragment;
+    private final int UPDATE_REQUEST = 2019;
+
+    @Override
+    protected void initializeFragment() {
+        initializeView();
+
+        btBack.setVisibility(View.GONE);
+        btOptionMenu.setVisibility(View.GONE);
+//        ivIcon.setImageResource(R.drawable.....);
+        ivIcon.setVisibility(View.VISIBLE);
+
+        profileFragment = new ProfileFragment();
+        setCurrentFragment(profileFragment, false);
+
+    }
+
+
+
+}
---
+++ BelajarActivity/app/src/main/java/pens/lab/app/belajaractivity/modul/profile/ProfileFragment.java
@@ -0,0 +1,73 @@
+package pens.lab.app.belajaractivity.modul.profile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import pens.lab.app.belajaractivity.FirstActivity;
+import pens.lab.app.belajaractivity.R;
+import pens.lab.app.belajaractivity.base.BaseFragment;
+
+
+/**
+ * Created by fahrul on 13/03/19.
+ */
+
+public class ProfileFragment extends BaseFragment<ProfileActivity, ProfileContract.Presenter> implements ProfileContract.View {
+
+    EditText etEmail;
+    EditText etPassword;
+    Button btnLogin;
+
+
+    public ProfileFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        fragmentView = inflater.inflate(R.layout.fragment_login, container, false);
+        mPresenter = new ProfilePresenter(this);
+        mPresenter.start();
+
+        etEmail = fragmentView.findViewById(R.id.et_email);
+        etPassword = fragmentView.findViewById(R.id.et_password);
+        btnLogin = fragmentView.findViewById(R.id.bt_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setBtLoginClick();
+            }
+        });
+
+        setTitle("Profile");
+
+        return fragmentView;
+    }
+
+    public void setBtLoginClick(){
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+        mPresenter.performLogin(email,password);
+    }
+
+    @Override
+    public void setPresenter(ProfileContract.Presenter presenter) {
+        mPresenter = presenter;
+    }
+
+    @Override
+    public void redirectToProfile() {
+            Intent intent = new Intent(activity, FirstActivity.class);
+            startActivity(intent);
+            activity.finish();
+    }
+
+
+}
